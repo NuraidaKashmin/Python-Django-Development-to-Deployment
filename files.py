dc27f4f344c82a1323690ef612abe82fd1fb5f50
NuraidaKashmin <nuraidakkashmin@gmail.com>
2025-02-27 02:39:04 +0600

    learned about files

--- files.py
+++ files.py
@@ -1,1 +1,26 @@
 # Python has functions for creating, reading, updating, and deleting files.
+
+
+# open a file
+myFile = open('myfile.txt', 'w')
+
+# get some info
+print('Name: ', myFile.name)
+print('Is Closed: ', myFile.closed)
+print('Opening Mode: ', myFile.mode)
+
+# write to file
+myFile.write('I am learning Python')
+myFile.write(' and I am loving it')
+myFile.close()
+
+# append to file
+myFile = open('myfile.txt', 'a')
+myFile.write('. I also like html, CSS')
+myFile.close()
+
+
+# read from file
+myFile = open('myfile.txt', 'r+')
+text = myFile.read(100)
+print(text)
